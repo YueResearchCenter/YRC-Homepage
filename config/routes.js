c957a707763721e12fb89e6f65fcee3619f67377
destec <catalyst.zhang@gmail.com>
2015-04-26 23:02:14 +0800

    edit route

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,14 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+
+  'get /login': 'AuthController.login',
+  'get /logout': 'AuthController.logout',
+  'get /register': 'AuthController.register',
+
+  'post /auth/local': 'AuthController.callback',
+  'post /auth/local/:action': 'AuthController.callback'
 
   /***************************************************************************
   *                                                                          *
